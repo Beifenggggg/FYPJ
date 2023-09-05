0effb8331212265f785b3f3f2ae98ea5093849b4
Beifenggggg <jameschee00@gmail.com>
2023-09-05 15:46:47 +0800

    commit 3

---
+++ FYP/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f; // Adjust this to control the movement speed.
+
+    void Update()
+    {
+        // Get input from the player's keyboard.
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate the movement direction.
+        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        // Calculate the movement vector and scale it by the moveSpeed.
+        Vector3 movement = moveDirection * moveSpeed * Time.deltaTime;
+
+        // Move the player.
+        transform.Translate(movement);
+    }
+}
