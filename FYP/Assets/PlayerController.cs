1d33c44d881e0633dec2f8b6edad150ad10a3ea0
tanjingyan <57163441+tanjingyan@users.noreply.github.com>
2023-09-12 22:42:12 +0800

    entrances and button

--- FYP/Assets/PlayerController.cs
+++ FYP/Assets/PlayerController.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-
 {
-    
     public float moveSpeed = 5.0f; // Adjust this to control the movement speed.
+    public float jumpForce = 10.0f; // The force applied when jumping.
+    private Rigidbody rb; // Reference to the Rigidbody component.
+    private bool isGrounded = true; // Flag to check if the player is grounded.
+
+    void Start()
+    {
+        // Get the Rigidbody component attached to the player.
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
@@ -23,5 +30,36 @@ public class PlayerController : MonoBehaviour
         // Move the player.
         transform.Translate(movement);
 
+        // Check for player jump input.
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            // Apply an upward force to simulate jumping.
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false; // Set the grounded flag to false to prevent multiple jumps in mid-air.
+        }
+    }
+
+    // Use FixedUpdate for physics-related checks.
+    void FixedUpdate()
+    {
+        // Check if the player is grounded.
+        CheckGrounded();
+    }
+
+    void CheckGrounded()
+    {
+        // Create a ray that points downward from the player's center.
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+
+        // Check if the ray hits something within a small distance.
+        if (Physics.Raycast(ray, out hit, 0.1f))
+        {
+            isGrounded = true; // The player is grounded.
+        }
+        else
+        {
+            isGrounded = false; // The player is not grounded.
+        }
     }
 }
