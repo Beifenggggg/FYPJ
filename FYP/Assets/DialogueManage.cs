a98ac77c955795a775b71fc5b0797c7bc31681ae
CHEE CHONG HE <211750Z@sit.nyp.edu.sg>
2023-09-15 17:23:35 +0800

    dsvdvscx

---
+++ FYP/Assets/DialogueManage.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Io.Pipes;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager: MonoBehaviour
+{
+    // Start is called before the first frame update
+  public NPC npc;
+
+  bool isTalking = false;
+
+  float distance;
+
+  public GameObject player;
+  public GameObject dialogueUi;
+
+  public Text npcName;
+  public Text npcDialoguebox;
+  public Text playerResponse;
+
+  void Start()
+  {
+    dialogueUI.SetActive(false)
+  }                                                                                                                                                                                                                                          
+}
